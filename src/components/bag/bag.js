a8353dd50a1f9f20be64228f665103c90dcb500f
NguyenNgocQuang2001 <sinhvienuetk64@gmail.com>
2022-11-02 23:46:22 +0700

    update components

---
+++ src/components/bag/bag.js
@@ -0,0 +1,13 @@
+import styles from "./bag.module.scss"
+
+function Bag() {
+
+    return (
+
+        <div className={styles.bag}>
+            
+        </div>
+    )
+}
+
+export default Bag
---
+++ src/components/navbarProfile/narbarProfile.js
@@ -0,0 +1,12 @@
+import styles from "./navbarProfile.module.scss"
+
+
+function NavbarProfile() {
+
+    return (
+
+        <div className={styles.navbar__profile}></div>
+    )
+}
+
+export default NavbarProfile
