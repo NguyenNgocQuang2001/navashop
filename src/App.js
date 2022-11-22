bba3a199b561cbe0494c3db81b3c9a6e82a1914f
NguyenNgocQuang2001 <sinhvienuetk64@gmail.com>
2022-11-22 22:39:56 +0700

    update introduce

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { Header } from './components/header';
 import { Route, Routes } from 'react-router-dom';
 import { Home } from './components/home';
@@ -15,8 +14,11 @@ import { AdminCustomer } from './components/adminCustomer';
 import { AdminStatistical } from './components/adminStatistical';
 import { Login } from './components/login';
 import { Signup } from './components/signup';
+import { Introduce } from "./components/introduce"
 
 function App() {
+
+
     return (
         <>
             <Header />
@@ -24,6 +26,7 @@ function App() {
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/home" element={<Home />} />
+                <Route path="/introduce" element={<Introduce />} />
                 <Route path="/product" element={<Home />} />
                 <Route path="/selling" element={<Home />} />
                 <Route path="/newproduct" element={<Home />} />
@@ -42,54 +45,6 @@ function App() {
             </Routes>
             <Footer />
         </>
-    );
-=======
-import { Header } from "./components/header";
-import { Route, Routes } from 'react-router-dom'
-import { Home } from "./components/home";
-import { ProductPage } from "./components/productDetail";
-import { Navbar } from "./components/navbar"
-import { Footer } from "./components/footer"
-import { UserBag } from "./components/userBag";
-import { UserHistory } from "./components/userHistory";
-import { UserOrder } from "./components/useOrder";
-import { UserProfile } from "./components/userProfile";
-import { AdminProfile } from "./components/adminProfile";
-import { AdminProduct } from "./components/adminProduct";
-import { AdminCustomer } from "./components/adminCustomer";
-import { AdminStatistical } from "./components/adminStatistical";
-import { Introduce } from "./components/introduce";
-
-function App() {
-
-	
-	return (
-		
-		<>
-			<Header />
-			<Navbar />
-			<Routes>
-				<Route path="/" element={<Home />} />
-				<Route path="/home" element={<Home />} />
-				<Route path="/product" element={<Home />} />
-				<Route path="/introduce" element={<Introduce />} />
-				<Route path="/selling" element={<Home />} />
-				<Route path="/newproduct" element={<Home />} />
-				<Route path="/sale" element={<Home />} />
-				<Route path="/product/:id" element={<ProductPage /> } />
-				<Route path="/:id/bag" element={<UserBag /> } />
-				<Route path="/:id/profile" element={<UserProfile /> } />
-				<Route path="/:id/order" element={<UserOrder /> } />
-				<Route path="/:id/history" element={<UserHistory /> } />
-				<Route path="/admin/profile" element={<AdminProfile /> } />
-				<Route path="/admin/addproduct" element={<AdminProduct /> } />
-				<Route path="/admin/customers" element={<AdminCustomer /> } />
-				<Route path="/admin/statistical" element={<AdminStatistical /> } />
-			</Routes>
-			<Footer />
-		</>
-	)
->>>>>>> 58dca5f799a037c109f45a70216da41b07bd8dfe
+    )
 }
-
 export default App;
