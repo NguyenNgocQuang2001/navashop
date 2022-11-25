23896a3fa9ebbeb6edd076c8786b0dbf2d69adbd
Sotatek-QuangNguyen5 <quang.nguyen5@sotatek.com>
2022-11-25 17:45:02 +0700

    update statistical

--- src/components/statistical/statistical.js
+++ src/components/statistical/statistical.js
@@ -1,5 +1,7 @@
 import styles from "./statistical.module.scss"
 import Select from "react-select"
+import { Link } from "react-router-dom"
+import { MyPagination } from "../pagination"
 
 
 const optionTime = [
@@ -26,6 +28,37 @@ const optionSex = [
     { value: '2', label: 'Do nam' },
     { value: '3', label: 'Do nu' },
     { value: '4', label: 'Do doi' },
+]
+
+const products = [
+    {
+        image: "/Image/aovetcongso.jpg",
+        name: "Áo Vest Công Sở",
+        price: 250000,
+        count: 1,
+        total: "250,000₫"
+    },
+    {
+        image: "/Image/aokhoackakihailop.jpg",
+        name: "Áo khoác kaki hai lớp",
+        price: 425000,
+        count: "2",
+        total: "850,000đ"
+    },
+    {
+        image: "/Image/aokhoackakihailop.jpg",
+        name: "Áo khoác kaki hai lớp",
+        price: 425000,
+        count: "2",
+        total: "850,000đ"
+    },
+    {
+        image: "/Image/aokhoackakihailop.jpg",
+        name: "Áo khoác kaki hai lớp",
+        price: 425000,
+        count: "2",
+        total: "850,000đ"
+    }
 ]
 
 function Statistical() {
@@ -61,7 +94,67 @@ function Statistical() {
                     options={optionSex}
                 />
             </div>
-            
+            <div
+                className={styles.sum__quantity}
+            >
+                Tong san pham da ban duoc :  119
+            </div>
+            <div
+                className={styles.sum__money}
+            >
+                Tong doanh thu :  9000.000VND
+            </div>
+            <div
+                className={styles.products}
+            >
+                {
+                    products.map((item, index) => {
+
+                        return (
+
+                            <div 
+                                className={styles.product__item}
+                                key={index}
+                            >
+                                <Link
+                                    to="/product/joker"
+                                >
+                                    <img
+                                        src={item.image}
+                                        alt="not found"
+                                        className={styles.image}
+                                    />
+                                </Link>
+                                <div
+                                    className={styles.info}
+                                >
+                                    <div
+                                        className={styles.name}
+                                    >
+                                        Ao cherry thoi trang
+                                    </div>
+                                    <div
+                                        className={styles.quantity}
+                                    >
+                                        So luong da ban duoc  &nbsp; : &nbsp; {item.count}
+                                    </div>
+                                    <div
+                                        className={styles.cost}
+                                    >
+                                        Gia tien &nbsp; : &nbsp; {item.price}
+                                    </div>
+                                    <div
+                                        className={styles.cost}
+                                    >
+                                        Tong tien &nbsp; : &nbsp; {item.price * item.count}
+                                    </div>
+                                </div>
+                            </div>
+                        )
+                    })
+                }
+            </div>
+            <MyPagination />
         </div>
     )
 }
